82c4991e4e0f8311d6b26e4b562be1ac217fe7db
Denis Fedorov <Denis_Fedorov@epam.com>
2018-07-31 20:32:53 +0300

    Add mock buttons for work with DB

--- main.py
+++ main.py
@@ -11,38 +11,51 @@ class Gui:
     def __init__(self, master):
         self.separator = "========================"
 
-        # common settings
+        # 1. common settings
         self.master = master
         self.master.title("HTML-tags ultimate counter")
         self.master.geometry("500x300")
 
-        # frames
-        self.controlFrame = Frame(self.master)
+        # 2. frames
+        self.upperFrame = Frame(self.master)
         self.outputFrame = Frame(self.master)
-        self.closeFrame = Frame(self.master)
+        self.bottomFrame = Frame(self.master)
 
-        # controls
-        self.labelUrl = Label(self.controlFrame, text="URL:")
-        defaulturl = StringVar(self.controlFrame, value="https://google.com")
-        self.entryUrl = Entry(self.controlFrame, textvariable=defaulturl, justify=LEFT)
-        self.buttonRun = Button(self.controlFrame, text="Run", command=self.run)
+        # 3. controls
+        # 3.1 upper frame
+        self.labelUrl = Label(self.upperFrame, text="URL:")
+        defaulturl = StringVar(self.upperFrame, value="https://google.com")
+        self.urlEntry = Entry(self.upperFrame, textvariable=defaulturl, justify=LEFT)
+        self.runButton = Button(self.upperFrame, text="Run", command=self.run)
+        # 3.2 output frame
         self.outputText = ScrolledText(self.outputFrame, wrap=WORD)
+        # 3.3 buttom frame
+        self.saveToDbButton = Button(self.bottomFrame, text="Save to DB", justify=LEFT, command=self.save_to_db)
+        self.loadFromDbButton = Button(self.bottomFrame, text="Load from DB", justify=LEFT, command=self.load_from_db)
+        self.exitButton = Button(self.bottomFrame, text="Exit", justify=RIGHT, command=self.exit)
 
-        # frames layout
-        self.controlFrame.pack(side=TOP, fill=X)
-        self.outputFrame.pack(side=BOTTOM, expand=True, fill=BOTH)
+        # 4. frames layout
+        self.upperFrame.pack(side=TOP, fill=X)
+        self.bottomFrame.pack(side=BOTTOM, fill=X)
+        self.outputFrame.pack(expand=True, fill=BOTH)
 
-        # controls layout
+        # 5. controls layout
+        # 5.1 upper frame
         self.labelUrl.pack(side=LEFT)
-        self.buttonRun.pack(side=RIGHT)
-        self.entryUrl.pack(fill=X)
+        self.runButton.pack(side=RIGHT)
+        self.urlEntry.pack(fill=X)
+        # 5.2 output frame
         self.outputText.pack(side=TOP, expand=True, fill=BOTH)
+        # 5.3 bottom frame
+        self.saveToDbButton.pack(side=LEFT)
+        self.loadFromDbButton.pack(side=LEFT)
+        self.exitButton.pack(side=RIGHT)
 
     def run(self):
         self.clear_output()
         self.append_datetime_to_output()
 
-        url = self.entryUrl.get()
+        url = self.urlEntry.get()
         if not Validator.isurlcorrect(url):
             self.append_text_to_output("Please provide a correct url")
             return
@@ -57,6 +70,17 @@ class Gui:
 
         result = TagCounter.get_tags(source)
         self.append_dic_to_output(result)
+
+    def exit(self):
+        self.master.destroy()
+
+    def save_to_db(self):
+        print("Save to db in progress")
+        pass
+
+    def load_from_db(self):
+        print("Load from db in progress")
+        pass
 
     def clear_output(self):
         self.outputText.delete(1.0, END)
